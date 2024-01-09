100a984bdb27951677963f9dc793f114899e2046
kirankumarkarmala1996 <108362315+kirankumarkarmala1996@users.noreply.github.com>
2024-01-09 17:59:11 +0530

    changes
    changes

---
+++ src/main/java/com/Demo/Uday.java
@@ -0,0 +1,14 @@
+package com.Demo;
+
+public class Uday {
+	
+	private int eId;
+	private String eName;
+	private String eJob;
+	private String eDepartment;
+	private float eSalary;
+
+	public static void main(String[] args) {
+		
+	}
+}
